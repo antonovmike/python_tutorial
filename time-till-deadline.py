12300bc3bfd8d5bfbb8c40c8e436e1175cd4940f
antonovmike <antonvma@outlook.com>
2022-11-30 12:57:09 +0500

    countdown app

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -5,4 +5,9 @@ input_list = user_input.split(":")
 
 goal = input_list[0]
 deadline = input_list[1]
-print(input_list)
+
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+# calculation how many days from now till deadline
+today_date = datetime.datetime.today()
+
+print(deadline_date - today_date)
