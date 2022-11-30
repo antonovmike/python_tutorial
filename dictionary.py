6e22072c8f65609cc8f803ed5bedf4ec3796d310
antonovmike <antonvma@outlook.com>
2022-11-30 10:18:05 +0500

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,20 @@
-calculation_to_units = 24
-name_of_unit = "hours"
 
 
-def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+def days_to_units(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days are {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
+    else:
+        return "unsupported unit"
 
 
 def validate_and_execute():
     try:
     
-        user_input_number = int(numb_of_days_element)
+        user_input_number = int(days_and_unit_dictionary["days"])
         if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
             print(calculated_value)
         elif user_input_number == 0:
             print("You entered a 0")
@@ -27,3 +30,5 @@ while user_input != "exit":
     print(days_and_unit)
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
     print(days_and_unit_dictionary)
+    print(type(days_and_unit_dictionary))
+    validate_and_execute()
