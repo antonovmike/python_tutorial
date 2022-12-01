c9c69752d70f93e823f1ae579e32aab32575ffd5
antonovmike <antonvma@outlook.com>
2022-12-01 14:11:03 +0500

    Encapsulation private atributes

--- encapsulation.py
+++ encapsulation.py
@@ -1,7 +1,20 @@
 class Person:
     def __init__(self, name):
-        self.name = name
-        self.age = 1
+        self.__name = name
+        self.__age = 1
+# self.age - an atribute
+# self.__age - a private atribute
+    def set_age(self, age):
+        if 1 < age < 120:
+            self.__age = age
+        else:
+            print("Invalid age")
+
+    def get_age(self):
+        return self.__age
+
+    def get_name(self):
+        return self.__name
 
     def display_info(self):
         print(f"Name: {self.name}\tAge: {self.age}")
