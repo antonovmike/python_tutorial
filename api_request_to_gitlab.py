4e7a948c822889bec620d359f1d46c3556788883
antonovmike <antonvma@outlook.com>
2022-12-01 09:44:45 +0500

    API request to GitLab

--- api_request_to_gitlab.py
+++ api_request_to_gitlab.py
@@ -1,8 +1,5 @@
 import requests
 
-response = requests.get("https://gitlab.com/api/v4/users/nanuchi/projects")
-# print(response.text)
-
 """
 Does not work if you use python command in terminal:
 
@@ -16,7 +13,12 @@ ModuleNotFoundError: No module named 'requests'
 Use python3
 """
 
+response = requests.get("https://gitlab.com/api/v4/users/nanuchi/projects")
+# print(response.text)
 # print(response.json())
 # print(response.json()[0])
 
 my_projects = response.json()
+
+for project in my_projects:
+    print(f"Project Name: {project['name']}\nProject Url: {project['web_url']}\n")
