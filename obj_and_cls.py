3cec6ddfb830a23de532230a2127cb5a641b1cb5
antonovmike <antonvma@outlook.com>
2022-12-01 08:58:52 +0500

    Objects and Classes

--- obj_and_cls.py
+++ obj_and_cls.py
@@ -1,12 +1,8 @@
-class User:
-    def __init__(self, email, name, password, current_job_title):
-        self.email
-        self.name
-        self.password
-        self.current_job_title
+from obj_and_cls_class_user import User
 
-    def change_password():
-        # do smth
-    
-    def change_job_title():
-        # do smth
+app_user_one = User("ma@ma.com", "Mike Antonov", "pswd1", "Unemployed")
+app_user_one.get_user_info()
+app_user_one.change_job_title("Backend developer")
+
+app_user_two = User("aa@aa.com", "Alice", "secret", "DevOps Engineer")
+app_user_two.get_user_info()
--- obj_and_cls_class_user.py
+++ obj_and_cls_class_user.py
@@ -14,9 +14,3 @@ class User:
     def get_user_info(self):
         print(f"User {self.name} currently works as {self.current_job_title}. You can contact them at {self.email}")
 
-app_user_one = User("ma@ma.com", "Mike Antonov", "pswd1", "Unemployed")
-app_user_one.get_user_info()
-app_user_one.change_job_title("Backend developer")
-
-app_user_two = User("aa@aa.com", "Alice", "secret", "DevOps Engineer")
-app_user_two.get_user_info()
