e6cbd6581d009a8e9cd41f70e399d2685dff8e1e
antonovmike <antonvma@outlook.com>
2022-12-01 08:18:14 +0500

    Objects and Classes

---
+++ obj_and_cls.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, email, name, password, current_job_title):
+        self.email
+        self.name
+        self.password
+        self.current_job_title
+
+    def change_password():
+        # do smth
+    
+    def change_job_title():
+        # do smth
