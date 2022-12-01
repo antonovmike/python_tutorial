eb0bd18925dd5ca366dcd1feac549e83817cc31b
antonovmike <antonvma@outlook.com>
2022-12-01 08:52:56 +0500

    Objects and Classes

--- obj_and_cls_class_user.py
+++ obj_and_cls_class_user.py
@@ -16,3 +16,7 @@ class User:
 
 app_user_one = User("ma@ma.com", "Mike Antonov", "pswd1", "Unemployed")
 app_user_one.get_user_info()
+app_user_one.change_job_title("Backend developer")
+
+app_user_two = User("aa@aa.com", "Alice", "secret", "DevOps Engineer")
+app_user_two.get_user_info()
