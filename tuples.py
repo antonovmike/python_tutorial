ac7323e2b42af5d63757cfd757872a7a86e506cc
antonovmike <antonvma@outlook.com>
2022-12-02 08:37:17 +0500

    Tuples

--- tuples.py
+++ tuples.py
@@ -1,3 +1,5 @@
+# Tuples are immutable
+
 alice = ("Alice", 30)
 print(alice)
 wesker = "Wesker", 35
@@ -5,5 +7,14 @@ print(wesker)
 
 some_list = ["Fruit", 70, "Peace"]
 some_tuple = tuple(some_list)
-print(some_tuple)
-print(len(some_tuple))
+print(some_tuple[2])
+print("Tuple lenght", len(some_tuple))
+
+name, age, company, position = ("Mike", 39, "Canonical", "software developer")
+print(name)
+print(age)
+print(position)
+print(company)
+
+mike = ("Mike", 39, "Canonical", "software developer")
+print(mike[:3]) # ('Mike', 39, 'Canonical')
