dbd0adca103083d3cd99a5cb3c44d43fae3e6203
antonovmike <antonvma@outlook.com>
2022-12-02 08:57:26 +0500

    Tuples

--- tuples.py
+++ tuples.py
@@ -18,3 +18,41 @@ print(company)
 
 mike = ("Mike", 39, "Canonical", "software developer")
 print(mike[:3]) # ('Mike', 39, 'Canonical')
+print(mike[1:3]) # (39, 'Canonical')
+
+
+def get_user():
+    name = "Maria Salomea Skłodowska-Curie"
+    age = 66
+    company = "Université de Paris"
+    return name, age, company
+
+
+user = get_user()
+print(user) # ("Tom", 37, "Google")
+
+
+def print_person(name, age, company):
+    print(f"Name: {name}  Age: {age}  Company: {company}")
+
+pierre = ("Pierre Curie", 46)
+print_person(*pierre, "la Sorbonne")
+
+
+iterate_tuple = ("one", 2, "three")
+
+for item in iterate_tuple:
+    print(item)
+
+i = 0
+while i < len(iterate_tuple):
+    print(iterate_tuple[i])
+    i += 1
+
+
+user = ("Mike", 39, "unemployed")
+name = "Mike"
+if name in user:
+    print("User's name is", name)
+else:
+    print("User has another name")
