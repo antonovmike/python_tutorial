72c025dea3603197493309bd5d5b223c4ec2b39f
antonovmike <antonvma@outlook.com>
2022-12-02 07:32:28 +0500

    Error handler

--- errors_handler.py
+++ errors_handler.py
@@ -1,8 +1,15 @@
 try:
-    number = int(input("Enter a number: "))
-    print("Your number is:", number)
-except:
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter a number: "))
+    print(number1, "/", number2, "=", number1/number2)
+
+except ValueError:
     print("Conversion failed")
+
+except ZeroDivisionError:
+    print("Attempt to divide a number by zero")
+except BaseException:
+    print("General exception")
 
 finally:
     print("Unit try has completed the execution")
