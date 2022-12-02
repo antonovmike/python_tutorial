40c2506e8cbf23b5990d8e7afe7d2c5902985462
antonovmike <antonvma@outlook.com>
2022-12-02 09:24:35 +0500

    Dictionaries

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,3 @@
-
-
 def days_to_units(num_of_days, conversion_unit):
     if conversion_unit == "hours":
         return f"{num_of_days} days are {num_of_days * 24} hours"
@@ -32,3 +30,45 @@ while user_input != "exit":
     print(days_and_unit_dictionary)
     print(type(days_and_unit_dictionary))
     validate_and_execute()
+
+
+# dictionary = { key0: value_a, key1: value_b }
+# Examples of dictionaries
+
+uzbekistani_writers = {
+    "+111111111": "Alisher Navoiy",
+    "+222222222": "Muhammad Yusuf Bayoniy",
+    "+333333333": "Abdulla Qodiriy"
+}
+
+print(uzbekistani_writers["+111111111"])
+
+uzbekistani_writers["+222222222"] = "Muhammad Yusuf Bayoniy"
+print(uzbekistani_writers["+222222222"])
+# Add new writer
+uzbekistani_writers["+444444444"] = "Munis Shermuhammad"
+
+key = "+444444444"
+if key in uzbekistani_writers:
+    writer = uzbekistani_writers[key]
+    print(writer)
+else:
+    print("Writer not found")
+
+
+users_list = [
+    ["+111111111", "Alisher Navoiy"],
+    ["+222222222", "Muhammad Yusuf Bayoniy"],
+    ["+333333333", "Abdulla Qodiriy"]
+]
+users_dict = dict(users_list)
+print(users_dict)
+
+
+users_tuple = (
+    ("+111111111", "Alisher Navoiy"),
+    ("+222222222", "Muhammad Yusuf Bayoniy"),
+    ("+333333333", "Abdulla Qodiriy")
+)
+users_dict = dict(users_tuple)
+print(users_dict)
