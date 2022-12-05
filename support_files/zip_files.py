e18ccb01ab2759f3931e0a74660ed6da5bf6af42
antonovmike <antonvma@outlook.com>
2022-12-05 08:36:10 +0500

    Read and write zip files

--- support_files/zip_files.py
+++ support_files/zip_files.py
@@ -78,3 +78,67 @@ with ZipFile("some.zip", "r") as myzip:
         else:  
             print(f"File: {item.filename}")
 """
+
+"""
+List of file's names:
+
+from zipfile import ZipFile
+ 
+with ZipFile("some.zip", "r") as myzip:
+    for item in myzip.namelist():
+        print(item)
+"""
+
+"""
+Get file info:
+
+from zipfile import ZipFile
+
+with ZipFile("some.zip", "r") as myzip:
+    try:
+        hello_file = myzip.getinfo("hello.txt")
+        print(hello_file.file_size)
+    except KeyError:
+        print("The file does not exist")
+"""
+
+# Extract files
+# extractall(path=None, members=None, pwd=None)
+
+"""
+Extract all files:
+
+from zipfile import ZipFile
+
+with ZipFile("some.zip", "r") as myzip:
+    myzip.extractall()
+    # OR
+    # myzip.extractall(path="some_folder")
+
+Extract one file:
+
+myzip.extract("hello.txt")
+"""
+
+"""
+Read file to bytes:
+
+from zipfile import ZipFile
+ 
+with ZipFile("some.zip", "r") as myzip:
+    content = myzip.read("hello.txt")
+    print(content)
+"""
+
+# Open file
+# open(name, mode='r', pwd=None, *, force_zip64=False)
+
+"""
+from zipfile import ZipFile
+ 
+with ZipFile("metanit.zip", "a") as myzip:
+    # Add new file "hello5.txt"
+    with myzip.open("hello5.txt", "w") as hello_file:
+        encoded_str = bytes("Python...", "UTF-8")
+        hello_file.write(encoded_str)
+"""
